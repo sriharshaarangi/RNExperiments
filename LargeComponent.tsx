eb1764b518c1c14186448569fc19e18d6348222a
Sri Harsha Arangi <arangisa@amazon.com>
2024-04-08 23:19:07 +0530

    Add Gluestack

--- LargeComponent.tsx
+++ LargeComponent.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View } from "react-native";
 import { PaperProvider, Text } from "react-native-paper";
+import { config } from "@gluestack-ui/config";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
+import {Text as GText} from "@gluestack-ui/themed";
 
 const LargeComponent = () => {
   return (
@@ -8,6 +11,9 @@ const LargeComponent = () => {
       <PaperProvider>
         <Text> Text from react-native-paper! </Text>
       </PaperProvider>
+      <GluestackUIProvider config={config}>
+        <GText>Hello World!</GText>
+      </GluestackUIProvider>
     </View>
   );
 };
