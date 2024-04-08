430e2dd4b498bf1b7360619c2b8b646f5048b1ae
Sri Harsha Arangi <arangisa@amazon.com>
2024-04-08 23:11:26 +0530

    Add RN paper

--- LargeComponentWrapper.tsx
+++ LargeComponentWrapper.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+
+// uncomment below line to load LargeComponent eagerly
+// import LargeComponent from "./LargeComponent";
 function LargeComponentWrapper() {
 
+  // comment below line to load LargeComponent lazily
   const LazyLargeComponent = require('./LargeComponent').default;
 
   return (
+    // comment below line to load LargeComponent lazily
     <LazyLargeComponent />
+
+    // uncomment below line to load LargeComponent eagerly
+    // <LargeComponent />
   );
 }
 export default LargeComponentWrapper;
