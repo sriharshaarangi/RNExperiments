948c27f636df03771828408476c99b51edc26de9
Sri Harsha Arangi <arangisa@amazon.com>
2024-04-15 18:03:23 +0530

    Require without lazy

--- LargeComponentWrapper.tsx
+++ LargeComponentWrapper.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import {Text} from 'react-native';
 function LargeComponentWrapper() {
-  const Component = React.lazy(
-    () => import(/* webpackChunkName: "largeComponent" */ './LargeComponent'),
-  );
-  return (
-    <React.Suspense fallback={<Text>Loading Remote Module...</Text>}>
-      <Component />
-    </React.Suspense>
-  );
+  const Component = require(/* webpackChunkName: "largeComponent" */ './LargeComponent');
+  return <Component />;
 }
 export default LargeComponentWrapper;
